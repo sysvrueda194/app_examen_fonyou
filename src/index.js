8b5545a11a59b7db14717514d164473367bfd5be
Victor Rueda <sysvrueda194@yahoo.com>
2022-10-20 09:13:37 -0500

    Version inicial de WebPack, Output Management

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Notes from './data/data.csv';
 import toml from './data/data.toml';
 import yaml from './data/data.yaml';
 import json from './data/data.json5';
+import printMe from './print.js';
 
 console.log(toml.title); // output `TOML Example`
 console.log(toml.owner.name); // output `Tom Preston-Werner`
@@ -16,11 +17,17 @@ console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, currently included via a script, is reqired for this line to work
     //Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     // Add the image to our existing div.
     const myIcon = new Image();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const toml = require('toml');
 const yaml = require('yamljs');
 const json5 = require('json5');
 
 module.exports = {
     mode: 'none',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
